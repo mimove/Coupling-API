d57e70a65e1b70c954842f37dbeb7687389e9453
mimove <41382474+mimove@users.noreply.github.com>
2022-05-04 15:58:19 +0200

    Update cwipiIcoFoam.C
    Include Lamb vector as the source term of APE

--- OpenFOAM_Nektar_API/cwipiIcoFoam/cwipiIcoFoam.C
+++ OpenFOAM_Nektar_API/cwipiIcoFoam/cwipiIcoFoam.C
@@ -1,97 +1,162 @@
-Info<< "Reading transportProperties\n" << endl;
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
 
-IOdictionary transportProperties
-(
-    IOobject
-    (
-        "transportProperties",
-        runTime.constant(),
-        mesh,
-        IOobject::MUST_READ_IF_MODIFIED,
-        IOobject::NO_WRITE
-    )
-);
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-dimensionedScalar nu
-(
-    "nu",
-    dimViscosity,
-    transportProperties.lookup("nu")
-);
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
 
-Info<< "Reading field p\n" << endl;
-volScalarField p
-(
-    IOobject
-    (
-        "p",
-        runTime.timeName(),
-        mesh,
-        IOobject::MUST_READ,
-        IOobject::AUTO_WRITE
-    ),
-    mesh
-);
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
+Application
+    icoFoam
 
-Info<< "Reading field U\n" << endl;
-volVectorField U
-(
-    IOobject
-    (
-        "U",
-        runTime.timeName(),
-        mesh,
-        IOobject::MUST_READ,
-        IOobject::AUTO_WRITE
-    ),
-    mesh
-);
+Description
+    Transient solver for incompressible, laminar flow of Newtonian fluids.
 
+\*---------------------------------------------------------------------------*/
 
-#include "createPhi.H"
+#include "cwipiPstream.H"
+#include "fvCFD.H"
+#include "pisoControl.H"
 
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-label pRefCell = 0;
-scalar pRefValue = 0.0;
-setRefCell(p, mesh.solutionDict().subDict("PISO"), pRefCell, pRefValue);
-mesh.setFluxRequired(p.name());
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
 
-    volVectorField L
-    (
-        IOobject
+    pisoControl piso(mesh);
+
+    #include "createFields.H"
+    #include "initContinuityErrs.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Switch cwipiSwitch = runTime.controlDict().lookup("cwipiSwitch");
+    scalar cwipiDim = readScalar(runTime.controlDict().lookup("cwipiDim"));
+    scalar cwipiStep = readScalar(runTime.controlDict().lookup("cwipiStep"));
+    scalar cwipiTimestep = cwipiStep;
+
+    if (cwipiSwitch)
+    {
+        cwipiCoupling(mesh, cwipiDim);
+    }
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.loop())
+    {
+        L = fvc::curl(U) ^ U;
+        const GeometricField<vector, fvPatchField, volMesh>& LMean_ =
+            L.db().objectRegistry::
+                lookupObject<GeometricField<vector, fvPatchField, volMesh> >
+                (
+                     "LMean"
+                );
+        LPrime = L - LMean_;
+
+        if (cwipiSwitch && cwipiTimestep == cwipiStep)
+        {
+            cwipiSend(mesh, LPrime, cwipiDim);
+        }
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        #include "CourantNo.H"
+
+        // Momentum predictor
+
+        fvVectorMatrix UEqn
         (
-            "L",
-            runTime.timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-        fvc::curl(U) ^ U
-    );
+            fvm::ddt(U)
+          + fvm::div(phi, U)
+          - fvm::laplacian(nu, U)
+        );
 
-    volVectorField LMean
-    (
-        IOobject
-        (
-            "LMean",
-            runTime.timeName(),
-            mesh,
-            IOobject::READ_IF_PRESENT,
-            IOobject::NO_WRITE
-        ),
-        fvc::curl(U) ^ U
-    );
+        if (piso.momentumPredictor())
+        {
+            solve(UEqn == -fvc::grad(p));
+        }
 
-    volVectorField LPrime
-    (
-        IOobject
-        (
-            "LPrime",
-            runTime.timeName(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-        fvc::curl(U) ^ U
-    );
+        // --- PISO loop
+        while (piso.correct())
+        {
+            volScalarField rAU(1.0/UEqn.A());
+            volVectorField HbyA(constrainHbyA(rAU*UEqn.H(), U, p));
+            surfaceScalarField phiHbyA
+            (
+                "phiHbyA",
+                fvc::flux(HbyA)
+              + fvc::interpolate(rAU)*fvc::ddtCorr(U, phi)
+            );
+
+            adjustPhi(phiHbyA, U, p);
+
+            // Update the pressure BCs to ensure flux consistency
+            constrainPressure(p, U, phiHbyA, rAU);
+
+            // Non-orthogonal pressure corrector loop
+            while (piso.correctNonOrthogonal())
+            {
+                // Pressure corrector
+
+                fvScalarMatrix pEqn
+                (
+                    fvm::laplacian(rAU, p) == fvc::div(phiHbyA)
+                );
+
+                pEqn.setReference(pRefCell, pRefValue);
+
+                pEqn.solve(mesh.solver(p.select(piso.finalInnerIter())));
+
+                if (piso.finalNonOrthogonalIter())
+                {
+                    phi = phiHbyA - pEqn.flux();
+                }
+            }
+
+            #include "continuityErrs.H"
+
+            U = HbyA - rAU*fvc::grad(p);
+            U.correctBoundaryConditions();
+        }
+
+        runTime.write();
+
+        if (cwipiSwitch && cwipiTimestep == cwipiStep)
+        {
+            Info<< "Before wait." << endl;
+            cwipiWait();
+            Info<< "After wait." << endl;
+            cwipiTimestep = 0;
+        }
+        cwipiTimestep = cwipiTimestep + 1;
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
